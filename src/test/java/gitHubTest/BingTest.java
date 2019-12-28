33498d760b461a3c93fa212e3a5127334a2675c1
Jayapratha15 <jpratha6@gmail.com>
2019-12-28 17:23:51 +0530

    All commit

---
+++ src/test/java/gitHubTest/BingTest.java
@@ -0,0 +1,30 @@
+package gitHubTest;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Test;
+
+public class BingTest {
+	
+	@Test
+	public void LoginTest() throws IOException	{
+		WebDriver driver = new FirefoxDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.google.com/");
+		File src = ((TakesScreenshot)driver).getScreenshotAs(OutputType.FILE);
+		FileUtils.copyFile(src, new File("C:\\\\Users\\\\Jaya\\\\eclipse-workspace\\\\JitHubTestt\\\\Screenshot\\\\Test1.jpeg"));
+
+		
+	}
+
+}
+
+
+	
+
